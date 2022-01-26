6897521364f203032ee546b7808954f83c3e7c56
Leonardo5284 <leonardogafer@gmail.com>
2022-01-26 13:11:52 -0600

    Juego con errores

--- Tech_With_Tim_Tutorial/main.py
+++ Tech_With_Tim_Tutorial/main.py
@@ -1,16 +1,42 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Game")
+
+WHITE = (255,255,255)
+
+FPS = 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('PyGame', 'Tech_With_Tim_Tutorial', 'Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_WIDTH))
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('PyGame', 'Tech_With_Tim_Tutorial', 'Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_WIDTH))
+
+def draw_window():
+    WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP,(300,100))
+    WIN.blit(RED_SPACESHIP,(200,100))
+    pygame.display.update() 
+
 
 def main():
-
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                
+        draw_window()
+
+
     pygame.quit()
 
 if __name__=="__main__":
