bc031c32b3b6f8ec2e5d0dccea711e4b90f6c341
Leonardo5284 <leonardogafer@gmail.com>
2022-01-26 11:44:28 -0600

    Avanze Juego

--- Tech_With_Tim_Tutorial/main.py
+++ Tech_With_Tim_Tutorial/main.py
@@ -1,2 +1,17 @@
 import pygame
 
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+    pygame.quit()
+
+if __name__=="__main__":
+    main()
