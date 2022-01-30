74293f7cca48eca5151dd37eaa806e8ab399468d
jjavierf64 <jjavierf64@estudiantec.cr>
2022-01-29 20:01:44 -0600

    Basic Menu

--- SquidShanty/main.py
+++ SquidShanty/main.py
@@ -2,7 +2,7 @@ import pygame
 import os
 
 
-from menu.py import *
+from menu import *
 
 
 pygame.font.init()
@@ -13,7 +13,13 @@ WIDTH, HEIGHT = 1000,600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Squid Shanty")
 
-#In Game Constants
+# Menu Elements
+MENU_BUTTONS={
+"START" : pygame.Rect(WIDTH//2 - 75, HEIGHT*1//4, 150, 80),
+"QUIT" : pygame.Rect(WIDTH//2 - 75, HEIGHT*3//4, 150, 80)
+}
+
+# InGame Constants
 FPS=60
 INITIAL_SEA_VEL = 5
 sea_vel = INITIAL_SEA_VEL
@@ -21,22 +27,20 @@ BULLET_VEL = 7
 MAX_BULLETS = 5
 
 
+#Color Pallette
+COLOR={
+"RED":(110, 7, 7),
+"BLUE":(29, 30, 51),
+"KHAKI":(201, 182, 149),
+"BROWN":(56, 37, 30),
+"DARK_BROWN":(31, 18, 14)
+}
 
+# Initial Mode
+mode="menu"
 
-#Images
-
-#Color Definitions
-RED=(110, 7, 7)
-BLUE=(29, 30, 51)
-KHAKI=(201, 182, 149)
-BROWN=(56, 37, 30)
-DARK_BROWN=(31, 18, 14)
-
-
-
-#Functions
-
-def main():
+# Functions
+def main(menu_selected = 0):
 
     clock = pygame.time.Clock()
     run = True
@@ -46,3 +50,14 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+        
+
+        if mode=="menu":
+            draw_window_menu(WIN, COLOR, MENU_BUTTONS, menu_selected)
+            menu_selected = handle_menu(event, menu_selected)
+    
+    main()
+
+
+if __name__ == "__main__":
+    main()
--- SquidShanty/menu.py
+++ SquidShanty/menu.py
@@ -1,4 +1,32 @@
-def draw_window_menu():
-    WIN.blit(RED, (0,0))
+import pygame
 
+
+
+# Assets
+
+
+
+def handle_menu(event, menu_selected):
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_DOWN and menu_selected < 1:
+            menu_selected += 1
+
+        if event.key == pygame.K_UP and menu_selected > 0:
+            menu_selected -= 1
+    print(menu_selected)
+    return menu_selected
+
+
+def draw_window_menu(WIN, COLOR, MENU_BUTTONS, menu_selected=0):
+    start_color = COLOR["KHAKI"]
+    quit_color = COLOR["KHAKI"]
+    if menu_selected == 0:
+        start_color = COLOR["BROWN"]
+    elif menu_selected == 1:
+        quit_color = COLOR["BROWN"]
+    
+    WIN.fill(COLOR["BLUE"])
+    
+    pygame.draw.rect(WIN, start_color, MENU_BUTTONS["START"])
+    pygame.draw.rect(WIN, quit_color, MENU_BUTTONS["QUIT"])
     pygame.display.update()
