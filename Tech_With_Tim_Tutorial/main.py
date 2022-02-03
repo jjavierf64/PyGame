d1788063471d6acec50cf299585bbb9a30d890ac
Leonardo5284 <leonardogafer@gmail.com>
2022-02-03 14:15:34 -0600

    Avance

--- Tech_With_Tim_Tutorial/main.py
+++ Tech_With_Tim_Tutorial/main.py
@@ -4,15 +4,13 @@ import pygame
 import os
 import random
 
-from PyGame.SquidShanty.main import BULLET_VEL
-
 RN = random.randint(1,4)
 RN2 = random.randint(1,1)
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First Game")
-#pygame.display.set_palette
+
             
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -22,6 +20,7 @@ BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
 FPS = 60
 VEL = 5
 BULLET_VEL = 7
+MAX_BULLETS = 3
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
@@ -83,13 +82,16 @@ def main():
                 run = False
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LCTRL:
-                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height/2 - 2, 10, 5)
+                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(
+                        yellow.x + yellow.width, yellow.y + yellow.height/2 - 2, 10, 5)
                     yellow_bullets.append(bullet)
 
-                if event.key == pygame.K_RCTRL:
-                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height/2 - 2, 10, 5)
-                    yellow_bullets.append(bullet)
+                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(
+                        red.x, red.y + red.height/2 - 2, 10, 5)
+                    red_bullets.append(bullet)
+        
         
         keys_pressed = pygame.key.get_pressed()
         yellow_handle_movement(keys_pressed, yellow)
