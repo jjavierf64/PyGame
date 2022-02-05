8f7e7d9f5fc8dcc560530658d7157fba268c1c07
Leonardo5284 <leonardogafer@gmail.com>
2022-02-05 11:35:38 -0600

    Avance

--- SquidShanty/menu.py
+++ SquidShanty/menu.py
@@ -5,28 +5,39 @@ import os
 pygame.mixer.init()
 
 
-BLOB = pygame.mixer.Sound(os.path.join('Assets', 'Assets_Grenade+1.mp3'))
+POP = pygame.mixer.Sound(os.path.join('Assets', 'Pop.mp3'))
+PIP = pygame.mixer.Sound(os.path.join('Assets', 'Pip.mp3'))
+QUIT_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Quit.mp3'))
 
 
 def handle_menu(event, MENU_BUTTONS, menu_selected):
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_DOWN and menu_selected < 2:   #Change limit as menu options change
             menu_selected += 1
+            PIP.play()
 
         if event.key == pygame.K_UP and menu_selected > 0:
             menu_selected -= 1
+            PIP.play()
     
         if event.key == pygame.K_RETURN:
             if menu_selected == 0:
                 pygame.event.post(pygame.event.Event(MENU_BUTTONS["TO_GAME"]))
+                POP.play()
+        
+                
+                
 
             if menu_selected == 1:
-                BLOB.play()
+                POP.play()
 
             if menu_selected == 2:
+                POP.play()
+                QUIT_SOUND.play()
                 pygame.display.update()
                 pygame.time.delay(1000)
                 pygame.quit()
+                
     
     return menu_selected
 
