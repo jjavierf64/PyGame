f1b39642f37e56e93bf724421067b96e63f12e81
jjavierf64 <jjavierf64@estudiantec.cr>
2022-03-09 10:35:09 -0600

    plato9

---
+++ plato9/main.py
@@ -0,0 +1,10 @@
+import pygame
+import os
+
+pygame.font.init()
+pygame.mixer.init()
+
+
+WIN_WIDTH, WIN_HEIGHT = 1000,600
+WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption("plato9")
